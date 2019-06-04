4a04dc0c4235018d5381a5304c231aa859a6df69
miguel <mig.suarez49@gmail.com>
2019-06-04 17:23:42 -0400

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -13,15 +13,16 @@ def call(imageName, imageTag, githubCredentialId, repoOwner, hostnameURL) {
         withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
           echo repoOwner
           echo envStagingRepo
-          def repoNotExists = sh(script: '''
-              curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/$envStagingRepo | jq 'contains({message: "Not Found"})'
-            ''', returnStdout: true)
-          echo repoNotExists
-          if(repoNotExists) {
-          sh(script: """
-              curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envStagingRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
-            """)
-          }
+          //def repoNotExists = sh(script: '''
+          //    curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/$envStagingRepo | jq 'contains({message: "Not Found"})'
+          //  ''', returnStdout: true)
+         // echo repoNotExists
+         // if(repoNotExists) {
+         // sh(script: """
+         //     curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envStagingRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
+         //   """)
+         // }
+            //this eends mods
           //curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":""}' https://api.github.com/orgs/ORGANISATION_NAME/repos
         }
         writeFile file: "deploy.yml", text: deployYaml
